d5a593c14c77510396eb6d9c3d54c983816e9950
JRThompson <JRThompson@wil.yearup.org>
2024-05-08 10:45:31 -0400

    little diddles

--- src/test/java/cohort15/PhoneBookTest.java
+++ src/test/java/cohort15/PhoneBookTest.java
@@ -4,6 +4,7 @@ import org.junit.jupiter.api.AfterAll;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 
+import java.util.ArrayList;
 import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
@@ -21,30 +22,29 @@ class PhoneBookTest {
         phoneTime.add("Jonathan Burgess","123-456-7890");
         phoneTime.add("Jonathan Burgess","123-123-1234");
         phoneTime.add("Johnny Bungo","330-492-1992");
-    }
-    @BeforeAll
-    static void addMultipleEntriesAtOnce()
-    {
         phoneTime.add("Newguy Newsly","191-232-4411","191-232-4412","191-232-4413");
     }
     @Test
     void addTests()
     {
-        assertFalse(phoneTime.getPhoneRecord().isEmpty(), "Empty phonebook");
+        assertFalse(phoneTime.getPhoneRecord().isEmpty(), "Phonebook failed to fill");
         assertEquals(true, phoneTime.hasEntry("Newguy Newsly"), "Failed to addAll");
         assertEquals(3,phoneTime.lookup("Johnny Bungo").size(),
                 "Adding multiple numbers to one key over multiple method calls failed");
         List<String> holdList = phoneTime.lookup("Johnny Bungo");
         phoneTime.remove("Johnny Bungo");
-        phoneTime.add("Johnny Bungo",holdList);
-        assertEquals(holdList,phoneTime.lookup("Johnny Bungo"),"adding a list of String failed.");
+        List<String> newList = new ArrayList<String>(holdList);
+        phoneTime.add("Johnny Bungo",newList); //Stream.toList to make sure it's new.
+        assertEquals(holdList,phoneTime.lookup("Johnny Bungo"),
+                "adding a list of numbers failed.");
     }
 
     @Test
     void lookup()
     {
         List<String> daList = phoneTime.getPhoneRecord().get("Newguy Newsly");
-        assertEquals(daList.stream().toList(), phoneTime.lookup("Newguy Newsly"),"Lookup failure");
+        assertEquals(daList.stream().toList(), phoneTime.lookup("Newguy Newsly"),
+                "Lookup failure");
     }
     @Test
     void reverseLookUp()
